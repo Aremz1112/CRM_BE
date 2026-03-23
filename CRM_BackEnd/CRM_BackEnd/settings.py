42cc423e15d9374f03512f33671f751e0a680d2f
dano-rex <danorex22@gmail.com>
2026-03-23 10:07:41 +0100

    linked the url and added the app in settings.py

--- CRM_BackEnd/CRM_BackEnd/settings.py
+++ CRM_BackEnd/CRM_BackEnd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CRM_BackEnd_app_v1',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
