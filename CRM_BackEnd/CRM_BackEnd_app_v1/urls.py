b6cb57ec10f201bf32c45b0dc9b51666fc48c5d0
dano-rex <danorex22@gmail.com>
2026-03-26 10:24:58 +0100

    modified Semi branch

--- CRM_BackEnd/CRM_BackEnd_app_v1/urls.py
+++ CRM_BackEnd/CRM_BackEnd_app_v1/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import LoginUser, RegisterCustomerAPIView, UpdateCustomerAPIView, DeleteCustomer, FindACustomerAPIView, FindALLCustomerAPIView, RegisterUserAPIView
+from .views import LoginUser, RegisterCustomer, UpdateCustomer, DeleteCustomer, FindACustomer, FindALLCustomer, RegisterUser
 
 
 urlpatterns = [
-    path('FindACustomer/<int:id>', FindACustomerAPIView.as_view(), name="this api is used to find a customer") ,
-    path('RegisterCustomer/<int:id>', RegisterCustomerAPIView.as_view(), name="this api is used to register a customer") ,
-    path('UpdateCustomer/<int:id>', UpdateCustomerrAPIView.as_view(), name="this api is used to update a customer record") ,
-    path('DeleteCustomer/<int:id>', DeleteCustomerAPIView.as_view(), name="this api is used to delete a customer record") ,
-    path('FindALLCustomer/<int:id>', FindALLCustomerAPIView.as_view(), name="this api is used to find all customer") ,
-    path('RegisterUser/<int:id>', RegisterUserAPIView.as_view(), name="this app is used to register a user") ,
+    path('FindACustomer/<int:id>', FindACustomer.as_view(), name="this api is used to find a customer") ,
+    path('RegisterCustomer/<int:id>', RegisterCustomer.as_view(), name="this api is used to register a customer") ,
+    path('UpdateCustomer/<int:id>', UpdateCustomer.as_view(), name="this api is used to update a customer record") ,
+    path('DeleteCustomer/<int:id>', DeleteCustomer.as_view(), name="this api is used to delete a customer record") ,
+    path('FindALLCustomer/<int:id>', FindALLCustomer.as_view(), name="this api is used to find all customer") ,
+    path('RegisterUser/<int:id>', RegisterUser.as_view(), name="this app is used to register a user") ,
     
     
 
