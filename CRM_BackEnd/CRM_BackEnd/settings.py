603302ec83b4d18f895ccdfc340b61739a696685
dano-rex <danorex22@gmail.com>
2026-03-30 10:21:49 +0100

    Remove JWT and REST framework settings
    Removed SIMPLE_JWT and REST_FRAMEWORK settings from Django settings.

--- CRM_BackEnd/CRM_BackEnd/settings.py
+++ CRM_BackEnd/CRM_BackEnd/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,22 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),  # Adjust this duration as needed
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'SIGNING_KEY': 'X-ACCESS-LOGIN-FOUNDATIONS-PROJECT',  # Ensure your signing key is securely managed
-    'VERIFYING_KEY': None,
-    'ALGORITHM': 'HS256',
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-}
-
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "user_mgt_v1.authentication.MongoJWTAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-    ),
-}
-
