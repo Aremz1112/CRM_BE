ae5c1e3da88354c59b57f1cdaa77f4d77cf87141
aremusemilore15-igtm <aremusemilore15@gmail.com>
2026-03-30 21:37:12 +0100

    modified Semi

---
+++ CRM_BackEnd/CRM_BackEnd_app_v1/authentication.py
@@ -0,0 +1,2 @@
+class MongoJWTAuthentication():
+    pass
--- CRM_BackEnd/CRM_BackEnd_app_v1/models.py
+++ CRM_BackEnd/CRM_BackEnd_app_v1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from mongoengine import Document, ListField, StringField, EmailField, DateField
 from datetime import datetime
+from django.contrib.auth.hashers import check_password, make_password
 
 # Create your models here.
 
@@ -9,6 +10,16 @@ class User(Document):
    email = EmailField(max_length=200)
    password = StringField(max_length=200)
    roles = ListField(StringField())
+
+   def set_password(self, password):
+      self.password = make_password(pwd)
+
+   def check_password(self, password):
+      return check_password(password, self.password)
+
+   @property
+   def is_authenticated(self):
+      return True
 
 class Customer(Document):
     custid= StringField(max_length=200)
