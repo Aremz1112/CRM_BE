03de2fedc2390de10df4689e49b80ae55a0c4db3
dano-rex <danorex22@gmail.com>
2026-04-06 11:10:07 +0100

    modified

--- CRM_BackEnd/CRM_BackEnd_app_v1/urls.py
+++ CRM_BackEnd/CRM_BackEnd_app_v1/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('UpdateCustomer/<str:id>', UpdateCustomer.as_view(), name="this api is used to update a customer record") ,
     path('DeleteCustomer/<str:id>', DeleteCustomer.as_view(), name="this api is used to delete a customer record") ,
     path('FindALLCustomer/<str:id>', FindALLCustomer.as_view(), name="this api is used to find all customer") ,
-    path('RegisterUser', RegisterUser.as_view(), name="this app is used to register a user") ,
+    path('RegisterUser', RegisterUser.as_view(), name="this api is used to register a user") ,
+    path('login', LoginUser.as_view(), name="this api is used to login a user") ,
     
     
 
